768c170619588dd5c18bd5739fb572f277f524b4
Roman Dubrovsky <roma2107946@gmail.com>
2015-10-15 16:02:55 +0300

    user 3 filters in one time

--- app/services/change_image_service.rb
+++ app/services/change_image_service.rb
@@ -2,7 +2,9 @@ class ChangeImageService
   def self.call model
     img = ChunkyPNG::Image.from_file(model.file.path)
 
+    img = Filters::MedianFilter.new.filtred(img)
     img = Filters::GaussFilter.new(img).filtered
+    img = Filters::RobertsOperatorFilter.new.filtred(img)
 
     img.save(model.file.path)
   end
