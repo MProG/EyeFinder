9058a66ba5d8f7e0399d9426e091fcc593e191d4
Max <shutkov.maksim@mail.ru>
2015-10-15 16:07:12 +0300

    Added roberts filter

--- app/services/filters/roberts_operator_filter.rb
+++ app/services/filters/roberts_operator_filter.rb
@@ -14,12 +14,28 @@ class Filters::RobertsOperatorFilter
   end
 
   def get_midle(x,y, img)
-    begin
-      tmp1 = (img[x,y] - img[x+1, y+1]).abs
-      tmp2 = (img[x+1,y] - img[x, y+1]).abs
-      Math.sqrt(tmp1**2+tmp2**2)
-    rescue
-      0
+    arr = to_rgb(img[x,y])
+    rez = {}
+
+    arr.each_key do |el|
+      begin
+        tmp1 = (to_rgb(img[x,y])[:"#{el}"] - to_rgb(img[x+1, y+1])[:"#{el}"]).abs
+        tmp2 = (to_rgb(img[x+1,y])[:"#{el}"] - to_rgb(img[x, y+1])[:"#{el}"]).abs
+      rescue
+        tmp1 = 0
+        tmp2 = 0
+      end
+      rez[:"#{el}"] = Math.sqrt(tmp1**2+tmp2**2).to_i
     end
+    ChunkyPNG::Color.rgb(rez[:currentRed], rez[:currentGreen], rez[:currentBlue])
   end
+
+  def to_rgb(pixel)
+    arr = {}
+    arr[:currentRed] = ChunkyPNG::Color.r(pixel)
+    arr[:currentGreen] = ChunkyPNG::Color.g(pixel)
+    arr[:currentBlue] = ChunkyPNG::Color.b(pixel)
+    arr    
+  end
+
 end
