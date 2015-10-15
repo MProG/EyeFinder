b89a01b332d079a5014692e80b7ab71a471fd10b
Roman Dubrovsky <roma2107946@gmail.com>
2015-10-15 15:57:06 +0300

    Remove unnesusary code

--- app/services/change_image_service.rb
+++ app/services/change_image_service.rb
@@ -2,15 +2,6 @@ class ChangeImageService
   def self.call model
     img = ChunkyPNG::Image.from_file(model.file.path)
 
-    # (0..array.dimension.width-1).each do |x|
-    #   (0..array.dimension.height-1).each do |y|
-    #     currentRed = ChunkyPNG::Color.r(array[x,y])
-    #     currentGreen = ChunkyPNG::Color.g(array[x,y])
-    #     currentBlue = ChunkyPNG::Color.b(array[x,y])
-
-    #     array[x,y] = ChunkyPNG::Color.rgb(0, 0, currentBlue)
-    #   end
-    # end
     img = Filters::GaussFilter.new(img).filtered
 
     img.save(model.file.path)
